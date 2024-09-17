470e3e41dd0ca79852e6eff701b5ba96db100f15
Kevin Saanen <kevin@saanen.me>
2024-09-17 13:26:25 +0200

    Add arguments

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
 extern crate colored;
 
 use colored::*;
-use std::fs::ReadDir;
+use std::fs::{DirEntry, ReadDir};
 use std::io::Result;
 
+struct Config {
+    all: bool,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let mut all = false;
+
+        for arg in args {
+            if arg == "-a" {
+                all = true;
+            }
+        }
+
+        Config { all }
+    }
+}
+
 fn main() {
+    let args = std::env::args().collect::<Vec<String>>();
+    let config = Config::new(&args);
+
     let files = read_current_dir();
     match files {
-        Ok(files) => print_files(files),
+        Ok(files) => print_files(files, &config),
         Err(_) => println!("Error reading directory"),
     }
 }
@@ -16,17 +37,39 @@ fn read_current_dir() -> Result<ReadDir> {
     std::fs::read_dir(".")
 }
 
-fn print_files(files: ReadDir) {
+fn print_files(files: ReadDir, config: &Config) {
     for file in files {
         if let Ok(file) = file {
-            if file.file_type().unwrap().is_dir() {
-                println!(
-                    "{}",
-                    file.path().display().to_string().truecolor(210, 168, 255)
-                );
-            } else {
-                println!("{}", file.path().display());
+            if is_hidden(&file) && !config.all {
+                continue;
+            }
+            let file_medatada = file.metadata();
+
+            if let Ok(file_medatada) = file_medatada {
+                if file_medatada.is_symlink() {
+                    // Check
+                    println!("{}", file.path().display().to_string().truecolor(255, 0, 0));
+                }
+                if file_medatada.is_file() {
+                    // Check
+                    println!("{}", file.path().display().to_string().truecolor(0, 255, 0));
+                }
+                if file_medatada.is_dir() {
+                    // Check
+                    println!(
+                        "{}",
+                        file.path().display().to_string().truecolor(210, 168, 255)
+                    );
+                }
             }
         }
     }
+}
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    let file_name = entry.file_name();
+    file_name
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
 }
